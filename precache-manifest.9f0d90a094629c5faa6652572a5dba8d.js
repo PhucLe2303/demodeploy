725f15a3caf1cd13c9a00a6f016b1555d7a36ed3
PhucIT <lehuuphuc23031999@gmail.com>
2020-04-26 22:21:48 +0700

    Updates

---
+++ precache-manifest.9f0d90a094629c5faa6652572a5dba8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62ca04cba92c90d4dfc66bd0febc75b3",
+    "url": "/demodeploy/index.html"
+  },
+  {
+    "revision": "19c2b58f449052547810",
+    "url": "/demodeploy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b6e1ea06999338a29e67",
+    "url": "/demodeploy/static/js/2.60a61667.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demodeploy/static/js/2.60a61667.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19c2b58f449052547810",
+    "url": "/demodeploy/static/js/main.30be7941.chunk.js"
+  },
+  {
+    "revision": "ed1b98ede72498ead38e",
+    "url": "/demodeploy/static/js/runtime-main.963aa8c6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demodeploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
